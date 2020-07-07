a7d5eebec36239894736f174f2946a725ab627cb
lukeemail <kjd1760@gmail.com>
2020-07-07 16:21:41 +0800

    feat: slug_airline_name

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,11 @@
 class Airline < ApplicationRecord
   has_many :reviews
+
+  before_create :slugify
+  # before_create http://blog-fred.logdown.com/posts/303186-before-action
+  def slugify
+    self.slug = name.parameterize
+    # slug https://medium.com/@hartaniyassir/creating-slug-urls-in-rails-without-gems-c693e0eeec8a
+    # parameterize https://apidock.com/rails/String/parameterize
+  end
 end
