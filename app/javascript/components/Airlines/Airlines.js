2dcffd1ab5852735b23f287d094ea6a49869ca90
lukeemail <kjd1760@gmail.com>
2020-07-08 17:57:49 +0800

    get: airlines_json_data

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect }from 'react'
+import axios from 'axios'
 
 const Airlines = () => {
   const [airlines, setAirlines] = useState([])
 
-  useEffect((()=> {
+  useEffect(()=> {
     // Get all of airlines from api
     // Update airlines in state
-  })
+
+  axios.get('/api/v1/airlines.json')
+  .then( resp => console.log(resp) )
+  .catch( resp => console.log(resp) )
+  ;}, [airlines.length]);
+
   return (
     <div>This is the Airlines#index view for app.</div>
   )
