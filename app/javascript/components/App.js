f4994ca38679a0839742227937f004139a482788
lukeemail <kjd1760@gmail.com>
2020-07-08 15:41:33 +0800

    set_up root path to point to an Airlines component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import {Route, Switch} from 'react-router-dom'
 const App = () => {
-  return (<switch></switch>)
+  return (
+  <switch>
+    <Route exact path="/" component={Airlines}/>
+  </switch>)
 }
 
 export default App
