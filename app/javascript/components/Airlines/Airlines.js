4ccb1edf78144acaf4d8b46ac230937a06f3784a
lukeemail <kjd1760@gmail.com>
2020-07-08 16:46:53 +0800

    import: useEffect to get api and update in state

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,7 +1,12 @@
-import React, { useState }from 'react'
+import React, { useState, useEffect }from 'react'
 
 const Airlines = () => {
   const [airlines, setAirlines] = useState([])
+
+  useEffect((()=> {
+    // Get all of airlines from api
+    // Update airlines in state
+  })
   return (
     <div>This is the Airlines#index view for app.</div>
   )
