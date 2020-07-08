c99c284046dc0afce500e38445e04c0bf8e49ca9
lukeemail <kjd1760@gmail.com>
2020-07-08 18:09:34 +0800

    import: fragment

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }from 'react'
+import React, { useState, useEffect, Fragment }from 'react'
 import axios from 'axios'
 
 const Airlines = () => {
@@ -9,12 +9,21 @@ const Airlines = () => {
     // Update airlines in state
 
   axios.get('/api/v1/airlines.json')
-  .then( resp => console.log(resp) )
+  .then( resp => {
+    setAirlines(resp.data.data)
+  })
   .catch( resp => console.log(resp) )
   ;}, [airlines.length]);
 
+  const list = airlines.map(item => {
+    return (<li key={item.attributes.name}>{item.attributes.name}</li>)
+  })
+
   return (
+    <Fragment>
     <div>This is the Airlines#index view for app.</div>
+    <li>list</li>
+    </Fragment>
   )
 }
 
