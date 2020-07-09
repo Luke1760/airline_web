3c215a33589a98e43bda5a216e13fc6bd32bd490
lukeemail <kjd1760@gmail.com>
2020-07-09 14:24:25 +0800

    create: grid for individual airline

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import {Route, Switch} from 'react-router-dom'
 import Airlines from './Airlines/Airlines'
-import airlines from './Airline/Airline'
+import Airline from './Airline/Airline'
 
 const App = () => {
   return (
   <switch>
     <Route exact path="/" component={Airlines}/>
-    <Route exact path="/airlines/:slug" component={Airlines}/>
+    <Route exact path="/airlines/:slug" component={Airline}/>
   </switch>
   )
 }
